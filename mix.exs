1254d7866187333d48b30ea9875512a1751cd06d
amattn <amattn@users.noreply.github.com>
2023-07-12 09:18:47 -0700

    package metadata

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Tuid.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "tuid",
+      source_url: "https://github.com/amattn/tuid"
     ]
   end
 
@@ -22,7 +26,24 @@ defmodule Tuid.MixProject do
   defp deps do
     [
       {:ecto, "~> 3.0"},
-      {:uniq, "~> 0.6"}
+      {:uniq, "~> 0.6"},
+      {:ex_doc, "~> 0.30", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Tagged, unique ids.  K-sortable, based on tagged, base58 encoded UUIDv7 IDs"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "tuid",
+      # These are the default files included in the package
+      files: ~w(lib test doc .formatter.exs mix.exs README*  LICENSE*
+                LICENSE CHANGELOG),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/amattn/tuid"}
     ]
   end
 end
